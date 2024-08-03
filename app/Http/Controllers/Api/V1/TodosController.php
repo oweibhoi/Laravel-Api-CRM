51cf5949c46c74db470b239f267f9ed5bf5efa9d
Joey <joeycatinza@tdsoftwareservices.com>
2024-08-03 17:01:04 +0800

    update todo controller

--- app/Http/Controllers/Api/V1/TodosController.php
+++ app/Http/Controllers/Api/V1/TodosController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use Illuminate\Support\Facades\DB;
 
 class TodosController extends Controller
 {
-    public function show($customer_id=0, $type = 'P')
+    public function show($customer_id = 0)
     {
+        $customerDetails = Customer::find($customer_id);
+        $type = $customerDetails->status == 2 ? 'C' : 'P';
+
         $todos = DB::table("settings_todos")
             ->where('settings_todos.status', 1)
             ->where('settings_todos.type', $type)
-            ->select('settings_todos.id', 'settings_todos.name', DB::raw('(SELECT id FROM customer_todos WHERE customer_todos.todo_id=settings_todos.id AND customer_todos.customer_id="'.$customer_id.'") as todos_id'))
+            ->select('settings_todos.id', 'settings_todos.name', DB::raw('(SELECT id FROM customer_todos WHERE customer_todos.todo_id=settings_todos.id AND customer_todos.customer_id="' . $customer_id . '") as todos_id'))
             ->get();
         return response()->json(collect($todos));
     }
